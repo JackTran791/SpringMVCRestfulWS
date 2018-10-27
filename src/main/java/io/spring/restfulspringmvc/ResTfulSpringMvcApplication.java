d497e6c58732a8ad8013b235254a02b71cb57e84
JackTran791 <congsonit@gmail.com>
2018-10-26 22:09:34 -0500

    Initial Set-up

---
+++ src/main/java/io/spring/restfulspringmvc/ResTfulSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package io.spring.restfulspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulSpringMvcApplication.class, args);
+    }
+}
---
+++ src/test/java/io/spring/restfulspringmvc/ResTfulSpringMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package io.spring.restfulspringmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResTfulSpringMvcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
